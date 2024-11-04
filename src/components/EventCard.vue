0052f1f16929224ff641c0665ed74f75e3c810be
pzy <pzyllll@outlook.com>
2024-11-04 14:31:00 +0800

    1.5

---
+++ src/components/EventCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+//import { ref } from "vue"
+import type { Event } from '@/types'
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <div class="event-card">
+ <RouterLink class="event-link" :to="{ name: 'event-detail-view', params:{ id: event.id } }">
+       <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    
+  </RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
